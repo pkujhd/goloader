df925ff7802dfc406bc54e9848309adc4a0c42ac
pkujhd <pkujhd@gmail.com>
2021-06-25 14:35:32 +0800

    update dynamic loader testcase

--- examples/soloader/run.sh
+++ examples/soloader/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 go build -o soloader.so -buildmode=c-shared soloader.go
-g++ -g -std=c++11 -ldl elf.cpp -w -o elf
-go tool compile soloadertest.go
+g++ -std=c++11 -ldl elf.cpp -w -o elf
+go tool compile -o soloadertest.o ../inline/inline.go
 ./elf
 
 rm -f soloader.h soloader.so soloadertest.o elf
