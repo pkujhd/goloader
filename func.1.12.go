27b01a49adb651c7966d41e068653110f2dc25b5
pkujhd <pkujhd@gmail.com>
2022-02-07 11:24:57 +0800

    remove duplicate _func code

--- func.1.12.go
+++ func.1.12.go
@@ -1,5 +1,5 @@
-//go:build go1.12 && !go1.14
-// +build go1.12,!go1.14
+//go:build go1.12 && !go1.16
+// +build go1.12,!go1.16
 
 package goloader
 
@@ -16,12 +16,16 @@ import (
 // Note: this list must match the list in cmd/internal/objabi/funcid.go.
 type funcID uint8
 
+// Layout of in-memory per-function information prepared by linker
+// See https://golang.org/s/go12symtab.
+// Keep in sync with linker (../cmd/link/internal/ld/pcln.go:/pclntab)
+// and with package debug/gosym and with symtab.go in package runtime.
 type _func struct {
 	entry   uintptr // start pc
 	nameoff int32   // function name
 
-	args int32 // in/out args size
-	_    int32 // previously legacy frame size; kept for layout compatibility
+	args        int32  // in/out args size
+	deferreturn uint32 // offset of start of a deferreturn call instruction from entry, if any.
 
 	pcsp      int32
 	pcfile    int32
@@ -29,20 +33,21 @@ type _func struct {
 	npcdata   int32
 	funcID    funcID  // set for certain special runtime functions
 	_         [2]int8 // unused
-	nfuncdata uint8
+	nfuncdata uint8   // must be last
 }
 
-func initfunc(symbol *obj.ObjSymbol, nameOff, spOff, pcfileOff, pclnOff, cuOff int) _func {
+func initfunc(symbol *obj.ObjSymbol, nameOff, spOff, pcfileOff, pclnOff int, cuOff int) _func {
 	fdata := _func{
-		entry:     uintptr(0),
-		nameoff:   int32(nameOff),
-		args:      int32(symbol.Func.Args),
-		pcsp:      int32(spOff),
-		pcfile:    int32(pcfileOff),
-		pcln:      int32(pclnOff),
-		npcdata:   int32(len(symbol.Func.PCData)),
-		funcID:    funcID(objabi.GetFuncID(symbol.Name, strings.TrimPrefix(symbol.Func.File[0], FileSymPrefix))),
-		nfuncdata: uint8(len(symbol.Func.FuncData)),
+		entry:       uintptr(0),
+		nameoff:     int32(nameOff),
+		args:        int32(symbol.Func.Args),
+		deferreturn: uint32(0),
+		pcsp:        int32(spOff),
+		pcfile:      int32(pcfileOff),
+		pcln:        int32(pclnOff),
+		npcdata:     int32(len(symbol.Func.PCData)),
+		funcID:      funcID(objabi.GetFuncID(symbol.Name, strings.TrimPrefix(symbol.Func.File[0], FileSymPrefix))),
+		nfuncdata:   uint8(len(symbol.Func.FuncData)),
 	}
 	return fdata
 }
