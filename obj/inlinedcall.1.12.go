9dc9b68e2965158a2f1481a266b81ae25b2b7908
Anonymous <anonymous@github.com>
2022-10-07 17:52:23 +0100

    Split previously pclntab into separate tables (as done in runtime) to assist in debugging + fix cutab layout to correctly offset when creating multi-object, multi-package linkers - previously stack traces had incorrect files due to incorrect cutab + filetab

--- obj/inlinedcall.1.12.go
+++ obj/inlinedcall.1.12.go
@@ -7,21 +7,21 @@ type funcID uint8
 
 // inlinedCall is the encoding of entries in the FUNCDATA_InlTree table.
 type InlinedCall struct {
-	parent   int16  // index of parent in the inltree, or < 0
+	parent   uint16 // index of parent in the inltree, or < 0
 	funcID   funcID // type of the called function
 	_        byte
-	file     int32 // fileno index into filetab
-	line     int32 // line number of the call site
-	func_    int32 // offset into pclntab for name of called function
-	parentPc int32 // position of an instruction whose source position is the call site (offset from entry)
+	file     uint32 // fileno index into filetab
+	line     uint32 // line number of the call site
+	func_    uint32 // offset into pclntab for name of called function
+	parentPc uint32 // position of an instruction whose source position is the call site (offset from entry)
 }
 
 func InitInlinedCall(inl InlTreeNode, funcid uint8, namemap map[string]int, filetab []uint32) InlinedCall {
 	return InlinedCall{
-		parent:   int16(inl.Parent),
+		parent:   uint16(inl.Parent),
 		funcID:   funcID(funcid),
 		file:     findFileTab(inl.File, namemap, filetab),
-		line:     int32(inl.Line),
-		func_:    int32(namemap[inl.Func]),
-		parentPc: int32(inl.ParentPC)}
+		line:     uint32(inl.Line),
+		func_:    uint32(namemap[inl.Func]),
+		parentPc: uint32(inl.ParentPC)}
 }
