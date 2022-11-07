958a899a0ba2a03dcd39226142c5cb0306295dce
Anonymous <anonymous@github.com>
2022-11-07 17:31:22 +0000

    Fix occasional error "go.info.runtime.itabTable: relocation target go.info.*github.com/pkujhd/goloader.itabTableType not defined" caused by linker not generating dwarf info for goloader.itabTableType correctly. Also fixes for go.info.runtime.itabLock

--- iface.1.10.go
+++ iface.1.10.go
@@ -29,27 +29,33 @@ type itabTableType struct {
 }
 
 //go:linkname itabTable runtime.itabTable
-var itabTable *itabTableType // pointer to current table
+var itabTable unsafe.Pointer // pointer to current table
+
+// Avoids "go.info.runtime.itabTable: relocation target go.info.*github.com/pkujhd/goloader.itabTableType not defined"
+var itabTableTyped = (*itabTableType)(itabTable)
 
 //go:linkname itabLock runtime.itabLock
-var itabLock mutex
+var itabLock uintptr
+
+// Avoids "go.info.runtime.itabLock: relocation target go.info.github.com/pkujhd/goloader.mutex not defined"
+var itabLockTyped mutex = mutex{itabLock}
 
 //go:linkname itabAdd runtime.itabAdd
 func itabAdd(m *itab)
 
 func additabs(module *moduledata) {
-	lock(&itabLock)
+	lock(&itabLockTyped)
 	for _, itab := range module.itablinks {
 		itabAdd(itab)
 	}
-	unlock(&itabLock)
+	unlock(&itabLockTyped)
 }
 
 func removeitabs(module *moduledata) bool {
-	lock(&itabLock)
-	defer unlock(&itabLock)
-	for i := uintptr(0); i < itabTable.size; i++ {
-		p := (**itab)(add(unsafe.Pointer(&itabTable.entries), i*PtrSize))
+	lock(&itabLockTyped)
+	defer unlock(&itabLockTyped)
+	for i := uintptr(0); i < itabTableTyped.size; i++ {
+		p := (**itab)(add(unsafe.Pointer(&itabTableTyped.entries), i*PtrSize))
 		m := (*itab)(loadp(unsafe.Pointer(p)))
 		if m != nil {
 			uintptrm := uintptr(unsafe.Pointer(m))
@@ -58,7 +64,7 @@ func removeitabs(module *moduledata) bool {
 			if (inter >= module.types && inter <= module.etypes) || (_type >= module.types && _type <= module.etypes) ||
 				(uintptrm >= module.types && uintptrm <= module.etypes) {
 				atomicstorep(unsafe.Pointer(p), unsafe.Pointer(nil))
-				itabTable.count = itabTable.count - 1
+				itabTableTyped.count = itabTableTyped.count - 1
 			}
 		}
 	}
