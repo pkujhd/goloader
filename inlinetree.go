35590baa796359987655af694180d8d2128e3b52
Anonymous <anonymous@github.com>
2023-05-27 15:10:07 +0100

    Bugfix inlinetree - attempt to use inlined func's funcID, otherwise assume normal

--- inlinetree.go
+++ inlinetree.go
@@ -1,20 +1,21 @@
 package goloader
 
 import (
+	"cmd/objfile/objabi"
 	"unsafe"
 
 	"github.com/eh-steve/goloader/obj"
 	"github.com/eh-steve/goloader/objabi/dataindex"
 )
 
-func (linker *Linker) addInlineTree(_func *_func, symbol *obj.ObjSymbol) (err error) {
+func (linker *Linker) addInlineTree(_f *_func, symbol *obj.ObjSymbol) (err error) {
 	funcname := symbol.Name
 	Func := symbol.Func
 	sym := linker.symMap[funcname]
 	if Func != nil && len(Func.InlTree) != 0 {
-		for _func.npcdata <= dataindex.PCDATA_InlTreeIndex {
+		for _f.npcdata <= dataindex.PCDATA_InlTreeIndex {
 			sym.Func.PCData = append(sym.Func.PCData, uint32(0))
-			_func.npcdata++
+			_f.npcdata++
 		}
 		sym.Func.PCData[dataindex.PCDATA_InlTreeIndex] = uint32(len(linker.pctab))
 
@@ -34,15 +35,25 @@ func (linker *Linker) addInlineTree(_func *_func, symbol *obj.ObjSymbol) (err er
 
 		bytes := make([]byte, len(Func.InlTree)*obj.InlinedCallSize)
 		for k, inl := range Func.InlTree {
-			inlinedcall := obj.InitInlinedCall(inl, getfuncID(_func), linker.namemap, linker.cutab)
+			// No special functions can be inlined in current versions of Go - we'll panic if they are
+			// If we can't find the inlined funcID, we assume it's FuncID_normal.
+			var funcID = uint8(objabi.FuncID_normal)
+			if inlSym, ok := linker.objsymbolMap[inl.Func]; ok {
+				funcID = inlSym.Func.FuncID
+				if funcID != 0 {
+					// This should never happen
+					panic("Unexpectedly non-zero funcID in inlined func: " + inl.Func + " inlined inside " + sym.Name)
+				}
+			}
+			inlinedcall := obj.InitInlinedCall(inl, funcID, linker.namemap, linker.cutab)
 			copy2Slice(bytes[k*obj.InlinedCallSize:], uintptr(unsafe.Pointer(&inlinedcall)), obj.InlinedCallSize)
 		}
 		offset := len(linker.noptrdata)
 		linker.noptrdata = append(linker.noptrdata, bytes...)
 		bytearrayAlign(&linker.noptrdata, PtrSize)
-		for _func.nfuncdata <= dataindex.FUNCDATA_InlTree {
+		for _f.nfuncdata <= dataindex.FUNCDATA_InlTree {
 			sym.Func.FuncData = append(sym.Func.FuncData, uintptr(0))
-			_func.nfuncdata++
+			_f.nfuncdata++
 		}
 		sym.Func.FuncData[dataindex.FUNCDATA_InlTree] = (uintptr)(offset)
 	}
