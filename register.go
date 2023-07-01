58bb448b91e1749565836879da8a95a4fd8b40ff
pkujhd <pkujhd@gmail.com>
2023-07-01 16:08:38 +0800

    fix #74, register Map/Func/Struct/Array/Slice/Chan type

--- register.go
+++ register.go
@@ -2,6 +2,7 @@ package goloader
 
 import (
 	"cmd/objfile/objfile"
+	"fmt"
 	"os"
 	"reflect"
 	"strings"
@@ -16,22 +17,7 @@ func typelinksRegister(symPtr map[string]uintptr) {
 		if md.typemap != nil {
 			t = (*_type)(adduintptr(md.typemap[typeOff(tl)], 0))
 		}
-		switch t.Kind() {
-		case reflect.Ptr:
-			name := t.nameOff(t.str).name()
-			element := *(**_type)(add(unsafe.Pointer(t), unsafe.Sizeof(_type{})))
-			pkgpath := t.PkgPath()
-			if element != nil && pkgpath == EmptyString {
-				pkgpath = element.PkgPath()
-			}
-			name = strings.Replace(name, pkgname(pkgpath), pkgpath, 1)
-			if element != nil {
-				symPtr[TypePrefix+name[1:]] = uintptr(unsafe.Pointer(element))
-			}
-			symPtr[TypePrefix+name] = uintptr(unsafe.Pointer(t))
-		default:
-			//NOTHING TODO
-		}
+		registerType(t, symPtr)
 	}
 	//register function
 	for _, f := range md.ftab {
@@ -44,6 +30,58 @@ func typelinksRegister(symPtr map[string]uintptr) {
 				}
 			}
 		}
+	}
+}
+
+func registerType(t *_type, symPtr map[string]uintptr) {
+	if t.Kind() == reflect.Invalid {
+		panic("Unexpected invalid kind during registration!")
+	}
+
+	pkgpath := t.PkgPath()
+	name := t.nameOff(t.str).name()
+	name = strings.Replace(name, pkgname(pkgpath), pkgpath, 1)
+
+	if t.tflag&tflagExtraStar != 0 {
+		name = name[1:]
+	}
+	name = TypePrefix + name
+	if _, ok := symPtr[name]; ok {
+		return
+	}
+	symPtr[name] = uintptr(unsafe.Pointer(t))
+
+	switch t.Kind() {
+	case reflect.Ptr, reflect.Chan, reflect.Array, reflect.Slice:
+		registerType(rtypeOf(t.Elem()), symPtr)
+	case reflect.Func:
+		for i := 0; i < t.NumIn(); i++ {
+			registerType(rtypeOf(t.In(i)), symPtr)
+		}
+		for i := 0; i < t.NumOut(); i++ {
+			registerType(rtypeOf(t.Out(i)), symPtr)
+		}
+	case reflect.Struct:
+		for i := 0; i < t.NumField(); i++ {
+			registerType(rtypeOf(t.Field(i).Type), symPtr)
+		}
+	case reflect.Map:
+		registerType(rtypeOf(t.Key()), symPtr)
+		registerType(rtypeOf(t.Elem()), symPtr)
+	case reflect.Bool,
+		reflect.Int, reflect.Uint,
+		reflect.Int64, reflect.Uint64,
+		reflect.Int32, reflect.Uint32,
+		reflect.Int16, reflect.Uint16,
+		reflect.Int8, reflect.Uint8,
+		reflect.Float64, reflect.Float32,
+		reflect.Complex64, reflect.Complex128,
+		reflect.String, reflect.UnsafePointer,
+		reflect.Uintptr,
+		reflect.Interface:
+		// Nothing to do
+	default:
+		panic(fmt.Sprintf("typelinksregister found unexpected type (kind %s): ", t.Kind()))
 	}
 }
 
--- type.go
+++ type.go
@@ -9,6 +9,8 @@ import (
 
 type tflag uint8
 
+const tflagExtraStar = 1 << 1
+
 // See reflect/value.go emptyInterface
 type emptyInterface struct {
 	typ  unsafe.Pointer
@@ -69,18 +71,55 @@ func _methods(t *uncommonType) []method
 //go:linkname _Kind reflect.(*rtype).Kind
 func _Kind(t *_type) reflect.Kind
 
+//go:linkname _NumField reflect.(*rtype).NumField
+func _NumField(t *_type) int
+
+//go:linkname _Field reflect.(*rtype).Field
+func _Field(t *_type, i int) reflect.StructField
+
+//go:linkname _NumIn reflect.(*rtype).NumIn
+func _NumIn(t *_type) int
+
+//go:linkname _In reflect.(*rtype).In
+func _In(t *_type, i int) reflect.Type
+
+//go:linkname _NumOut reflect.(*rtype).NumOut
+func _NumOut(t *_type) int
+
+//go:linkname _Out reflect.(*rtype).Out
+func _Out(t *_type, i int) reflect.Type
+
+//go:linkname _Key reflect.(*rtype).Key
+func _Key(t *_type) reflect.Type
+
+//go:linkname _Elem reflect.(*rtype).Elem
+func _Elem(t *_type) reflect.Type
+
 //go:linkname resolveNameOff runtime.resolveNameOff
 func resolveNameOff(ptrInModule unsafe.Pointer, off nameOff) name
 
 //go:linkname typelinksinit runtime.typelinksinit
 func typelinksinit()
 
-func (t *_type) uncommon() *uncommonType    { return _uncommon(t) }
-func (t *_type) nameOff(off nameOff) name   { return _nameOff(t, off) }
-func (t *_type) typeOff(off typeOff) *_type { return _typeOff(t, off) }
-func (n name) name() string                 { return _name(n) }
-func (t *uncommonType) methods() []method   { return _methods(t) }
-func (t *_type) Kind() reflect.Kind         { return _Kind(t) }
+func (t *_type) uncommon() *uncommonType         { return _uncommon(t) }
+func (t *_type) nameOff(off nameOff) name        { return _nameOff(t, off) }
+func (t *_type) typeOff(off typeOff) *_type      { return _typeOff(t, off) }
+func (n name) name() string                      { return _name(n) }
+func (t *uncommonType) methods() []method        { return _methods(t) }
+func (t *_type) Kind() reflect.Kind              { return _Kind(t) }
+func (t *_type) NumField() int                   { return _NumField(t) }
+func (t *_type) Field(i int) reflect.StructField { return _Field(t, i) }
+func (t *_type) NumIn() int                      { return _NumIn(t) }
+func (t *_type) In(i int) reflect.Type           { return _In(t, i) }
+func (t *_type) NumOut() int                     { return _NumOut(t) }
+func (t *_type) Out(i int) reflect.Type          { return _Out(t, i) }
+func (t *_type) Key() reflect.Type               { return _Key(t) }
+func (t *_type) Elem() reflect.Type              { return _Elem(t) }
+
+func rtypeOf(i reflect.Type) *_type {
+	eface := (*emptyInterface)(unsafe.Pointer(&i))
+	return (*_type)(eface.word)
+}
 
 func pkgname(pkgpath string) string {
 	slash := strings.LastIndexByte(pkgpath, '/')
