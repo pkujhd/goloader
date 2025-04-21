855e7565e204dfc2da50de48859363ce2a746f80
pkujhd <pkujhd@gmail.com>
2025-04-21 13:36:20 +0800

    add constants InvalidTypeOff, InvalidMethodOff and UnReachableTypeOff

---
+++ constants/typeoff.1.16.go
@@ -0,0 +1,8 @@
+//go:build go1.16 && !go1.25
+// +build go1.16,!go1.25
+
+package constants
+
+const InvalidTypeOff = 0
+const UnReachableTypeOff = -1
+const InvalidMethodOff = -1
---
+++ constants/typeoff.1.8.go
@@ -0,0 +1,8 @@
+//go:build go1.8 && !go1.16
+// +build go1.8,!go1.16
+
+package constants
+
+const InvalidTypeOff = 0
+const InvalidMethodOff = 0
+const UnReachableTypeOff = 0
