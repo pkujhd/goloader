0c30feb9484b144cb27a770b15218d6519be2780
pkujhd <pkujhd@gmail.com>
2025-04-21 13:35:47 +0800

    add typenamedata constant

--- constants/type.1.20.go
+++ constants/type.1.20.go
@@ -5,6 +5,7 @@ package constants
 
 const (
 	TypeImportPathPrefix = "type:.importpath."
+	TypeNameDataPrefix   = "type:.namedata."
 	TypeDoubleDotPrefix  = "type:."
 	TypePrefix           = "type:"
 	ItabPrefix           = "go:itab."
