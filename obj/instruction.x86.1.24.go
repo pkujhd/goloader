1eb097396fc60050fd379491188489116f3218b8
pkujhd <pkujhd@gmail.com>
2025-12-24 21:02:23 +0800

    support golang 1.26

--- obj/instruction.x86.1.24.go
+++ obj/instruction.x86.1.24.go
@@ -1,7 +1,7 @@
-//go:build (386 || amd64) && go1.24 && !go1.26
+//go:build (386 || amd64) && go1.24 && !go1.27
 // +build 386 amd64
 // +build go1.24
-// +build !go1.26
+// +build !go1.27
 
 package obj
 
