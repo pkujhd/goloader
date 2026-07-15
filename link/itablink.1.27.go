3f823e7d09aba7346d3836c859daa71bc0ec9b43
pkujhd <pkujhd@gmail.com>
2026-07-15 23:37:44 +0800

    adapt itablink for golang 1.27

---
+++ link/itablink.1.27.go
@@ -0,0 +1,37 @@
+//go:build go1.27 && !go1.28
+// +build go1.27,!go1.28
+
+package link
+
+import (
+	"unsafe"
+
+	"github.com/pkujhd/goloader/constants"
+)
+
+// Size returns the size of the itab in memory.
+func (it *itab) Size() int {
+	size := int(unsafe.Sizeof(itab{}))
+	if it.fun[0] == 0 {
+		return size
+	}
+	return size + (len(it.inter.mhdr)-1)*constants.PtrSize
+}
+
+func additabs(module *moduledata) {
+	lock(itabLock)
+	p := module.types + module.itaboffset
+	end := p + module.itabsize
+	for p < end {
+		it := (*itab)(unsafe.Pointer(p))
+		itabAdd(it)
+		p += uintptr(it.Size())
+	}
+	unlock(itabLock)
+}
+
+func (linker *Linker) AddItabLink(codeModule *CodeModule, symbolMap map[string]uintptr) {
+	module := codeModule.module
+	module.itaboffset = uintptr(codeModule.noPtrTypeDataLen)
+	module.itabsize = uintptr(codeModule.noPtrItabDataLen)
+}
